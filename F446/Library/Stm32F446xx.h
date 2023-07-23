5a538bcb029a62f70e6c787a83da138d3dd58319
Abdalrahman Sabaa <99027893+abdosaba22@users.noreply.github.com>
2023-07-23 17:22:53 +0300

    Add SCB

--- F446/Library/Stm32F446xx.h
+++ F446/Library/Stm32F446xx.h
@@ -19,6 +19,9 @@
 
 #define NVIC_BASE_ADDRESS			0xE000E100UL
 	
+/******************* SCB Peripheral Base Addresses ****************/
+
+#define SCB_BASE_ADDRESS		0xE000ED00UL
 
  
 /********************* AHB1 Peripheral Base Addresses *****************/
@@ -117,6 +120,27 @@ typedef struct {
   volatile uint32_t CALIB;     /*!< SYSTICK Calibration Value Register,                         */	
 } SYSTICK_RegDef_t;
 
+/************************** SCB Register Definition Structure ***********************/
+
+typedef struct
+{
+	volatile uint32_t CPUID;					/*CPUID base register*/
+	volatile uint32_t ICSR;                  /*Interrupt control and state register*/
+	volatile uint32_t VTOR;                  /*Vector table offset register*/
+	volatile uint32_t AIRCR;                 /*Application interrupt and reset control register*/
+	volatile uint32_t SCR;                   /*System control register*/
+	volatile uint32_t CCR;                   /*Configuration and control register*/
+	volatile uint32_t SHPR[3];               /*System handler priority registers*/
+	volatile uint32_t SHCSR;                 /*System handler control and state register*/
+	volatile uint32_t CFSR;                  /*Configurable fault status register*/
+	volatile uint32_t HFSR;                  /*Hard fault status register*/
+	volatile uint32_t Reserved;
+	volatile uint32_t MMAR;                  /*Memory management fault address register*/
+	volatile uint32_t BFAR;                  /*Bus fault status register*/
+	volatile uint32_t AFSR;                  /*Auxiliary fault status register*/
+}SCB_RegDef_t;
+
+
 
 /************************* NVIC Register Definition Structure ***********************/
 
@@ -185,7 +209,11 @@ typedef struct
 
 #define SYSTICK 			 ((SYSTICK_RegDef_t*) SYSTICK_BASE_ADDRESS)
 
-/************************ NVIC Definition **********************/
+/************************ SCB Peripheral Definition **********************/
+
+#define SCB					((SCB_RegDef_t*) SCB_BASE_ADDRESS)
+
+/************************ NVIC Peripheral Definition **********************/
 
 #define NVIC				 ((NVIC_RegDef_t*) NVIC_BASE_ADDRESS)
 
